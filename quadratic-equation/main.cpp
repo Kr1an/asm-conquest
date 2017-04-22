5c871230c55051482d1956be5ec010889c7b826a
Anton <kr1an@hotmail.com>
2017-04-22 13:16:58 +0000

    quadratic-equation/feature: add input option to main.cpp

--- quadratic-equation/main.cpp
+++ quadratic-equation/main.cpp
@@ -10,16 +10,21 @@ int main()
 	double rootfirst = 0;
 	double rootsecond = 0;
 	int condition = 0;
-	
-
+	printf("\tCalculate equation: a*x^2 + b*x + c = 0.\n");	
+	printf("Enter a: ");
+	scanf("%lf", &a);
+	printf("Enter b: ");
+	scanf("%lf", &b);
+	printf("Enter c: ");
+	scanf("%lf", &c);	
 	condition = findroots(a, b, c, &rootfirst, &rootsecond);
 	printf("\tEquation Calculation\n");
 	if(condition == 0)
 		printf("No real roots in equation\n");
 	if(condition == 1)
-		printf("First root: %f\nSecond root: %f\n", rootfirst, rootsecond);
+		printf("First root: %.2lf\nSecond root: %.2lf\n", rootfirst, rootsecond);
 	if(condition == 2)
-		printf("Root: %f\n", rootfirst);
+		printf("Root: %.2lf\n", rootfirst);
 	
 
 	return 0;
