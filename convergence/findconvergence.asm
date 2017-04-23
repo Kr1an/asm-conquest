5b0d73d1f219d50c65e71c913f22018248667920
Anton <kr1an@hotmail.com>
2017-04-23 13:39:12 +0000

    findconvergence/feature: add findsequencemembervalue procedure

--- convergence/findconvergence.asm
+++ convergence/findconvergence.asm
@@ -6,11 +6,18 @@
 
 	global findconvergence
 section .data
-	testv: dq 12.2
+	testmsg: db "loop iteration", 10
+	testmsg_len: equ $-testmsg
+	testp: dq 2.0
+	testv: dq 3.0
 section .bss
+	position: resq 1
 	argument: resq 1
 	functionvalue: resq 1
-	sequencevalue: resq 1
+	sequencemembervalue: resq 1
+	tmpvalue: resq 1
+	count: resq 1
+	trash: resq 1
 	
 section .text
 findconvergence:
@@ -20,10 +27,12 @@ findconvergence:
 	
 	fld qword [testv]
 	fstp qword [argument]
-	call findfunctionvalue
+	fld qword [testp]
+	fstp qword [position]
+	call findsequencemembervalue
 
 	mov ebx, t
-	fld qword [functionvalue]
+	fld qword [sequencemembervalue]
 	fstp qword [ebx]
 	
 	mov eax, 1	
@@ -56,15 +65,51 @@ findfunctionvalue:
 	pop ebp
 	ret
 
-findsequencevalue:
+findsequencemembervalue:
 ;Arguments:
-;	argument: value of x: should be qword
+;	argument: value of x, should be qword
+;	position: position of needed memder, should be qword
 ;Returns:
-;	sequencevalue: value of S(x), qword value
+;	sequencemembervalue: value of S(x), qword value
 	push ebp
 	mov ebp, esp
+	push ecx	
 
-
+	fld qword [argument]
+	fld1
+	fsubp st1, st0
+	
+	fld qword [argument]
+	fld1
+	faddp st1, st0
+	fdivp st1, st0
+	
+	fld qword [position]
+	fld qword [position]
+	faddp st1, st0
+	fld1
+	faddp st1, st0
+	fistp qword [count]
+	mov ecx, [count]
+	
+	fstp qword [tmpvalue]	
+	fld1
+findroot:	
+	fld qword [tmpvalue]
+	fmulp st1, st0
+	loop findroot 	
+	fld1
+	fld qword [position]
+	fld1
+	fld1
+	faddp st1, st0
+	fmulp st1, st0
+	fld1
+	faddp st1, st0
+	fdivp st1, st0
+	fmulp st1, st0	
+	fstp qword [sequencemembervalue]
+	pop ecx
 	mov esp, ebp
 	pop ebp
 	ret
