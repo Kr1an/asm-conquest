7f62955a11c58ce6cbd6015183c69b6338a09c3f
Anton <kr1an@hotmail.com>
2017-04-24 12:35:20 +0000

    convergence/feature: live become easier

--- convergence/findsval.asm
+++ convergence/findsval.asm
@@ -7,13 +7,16 @@ global findsval
 
 segment .data
 	cnt:	dq 0
-	sval:	dq 0.0	
+	sval:	dq 0.0
+	testv: 	dq 0.1
+	
 segment .bss
 	i:	resq 1
 	arg:	resq 1
 	eps:	resq 1
 	fval:	resq 1
 	tmpv: 	resq 1
+	trash:	resq 1
 segment .text
 findsval:
 	push ebp
@@ -22,80 +25,97 @@ findsval:
 	fld arg_def
 	fld eps_def
 	fld fval_def
+	fldz
+	fldz
+	fldz
+	fistp qword [i]
+	fistp qword [cnt]
+	fstp qword [sval]
 	fstp qword [fval]
 	fstp qword [eps]
 	fstp qword [arg]
+	
 converg:
-	
-	fld qword [arg]
-	fld1
-	fsubp st1, st0
 
-	fld qword [arg]
-	fld1
-	faddp st1, st0
-
-	fdivp st1, st0
-
+	fld qword [testv]
 	fild qword [cnt]
-	fild qword [cnt]
-	faddp st1, st0
-	fld1
-	faddp st1, st0
-
-	fistp ecx
-	
-	fstp qword [tmpv]
-	fld1
-powloop:
-	fld qword [tmpv]
 	fmulp st1, st0
-	loop powloop
-
-	fld1
-	
-	fild qword [count]
-	fld1
-	fld1
-	faddp st1, st0
-	fmulp st1, st0
-
-	fld1
-	faddp st1, st0
-
-	fdivp st1, st0
-
-	fmulp st1, st0
-
-	fld qword [sval]
-	faddp st1, st0
-
 	fstp qword [sval]
+	
+;	fld qword [arg]
+;	fld1
+;	fsubp st1, st0
+;
+;	fld qword [arg]
+;	fld1
+;	faddp st1, st0
+;
+;	fdivp st1, st0
+
+;	fild qword [cnt]
+;	fild qword [cnt]
+;	faddp st1, st0
+;	fld1
+;	faddp st1, st0
+;
+;	fistp qword [i]
+;	mov ecx, [i]
+;	
+;	fstp qword [tmpv]
+;	fld1
+;powloop:
+;	fld qword [tmpv]
+;	fmulp st1, st0
+;	loop powloop
+;
+;	fld1
+;	
+;	fild qword [cnt]
+;	fld1
+;	fld1
+;	faddp st1, st0
+;	fmulp st1, st0
+;
+;	fld1
+;	faddp st1, st0
+;
+;	fdivp st1, st0
+;
+;	fmulp st1, st0
+;
+;	fld qword [sval]
+;	faddp st1, st0
+;
+;	fstp qword [sval]
 assert:
 	fld qword [fval]
 	fld qword [sval]
 	fsubp st1, st0
 	fabs
 	fld qword [eps]
-	fcompp
+	fsubp st1, st0
+	;jmp exit
+	
 
 	ftst
-        fstsw ax
-        sahf
-	
-	jle found
+	fstsw ax
+	sahf
+	fstp qword [trash]
+
+	jb found
 inc:
-	fild qword [cnd]
+	fild qword [cnt]
 	fld1
 	faddp st1, st0
-	fistp qword [cnd]
+	fistp qword [cnt]
 
 	jmp converg
 found:
 	mov eax, sval_def
+exit:
 	fld qword [sval]
 	fstp qword [eax]
-	mov eax, cnt
+	mov eax, [cnt]
 	
 	mov esp, ebp
 	pop ebp
--- convergence/main.cpp
+++ convergence/main.cpp
@@ -10,6 +10,9 @@ int main()
 	double h = 0;
 	double e = 0;
 	double t = 0;
+	double fval = 0;
+	double sval = 0;
+	int seqmembcnt = 0;
 
 	printf("\tCalculate convergation: interval - [a, b], step - h, accuracy - e\n");	
 	printf("Enter a: ");
@@ -23,9 +26,9 @@ int main()
 
 	for(double x = a; x <= b; x += h)
 	{
-		int seqmembcnt;
-		double fval;
-		double sval;
+		seqmembcnt = 0;
+		fval = 0;
+		sval = 0;
 		findfval(x, &fval);
 		seqmembcnt = findsval(x, fval, e, &sval);
 		printf("| x: %lf\t| F: %lf\t| S: %lf\t| n: %d\t\n", x, fval, sval, seqmembcnt);
