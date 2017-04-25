a036f923d4ba1a0644ddc983b89795b96ebd54c3
Anton <kr1an@hotmail.com>
2017-04-25 08:05:58 +0300

    array-operations/feature: change everything to work with shorts

--- array-operations/main.cpp
+++ array-operations/main.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-extern "C" void sum(int *,int *); // mutate first arg
-extern "C" void mul(int *,int *); // mutate first arg
+extern "C" void sum(short *,short *); // mutate first arg
+extern "C" void mul(short *,short *); // mutate first arg
 
 
-int A[]={1,-2,3,200,5,6,7,8}; // values range [-254..255]
-int B[]={2,-3,4,200,6,7,8,8}; // values range [-254..255]
-int C[]={3,4,5,6,7,8,9,8}; // values range [-254..255]
-int D[]={1,3,2,5,6,7,8,8}; // values range [-2^16+1..2^16]
+short A[]={1,-2,3,200,5,6,7,8}; // values range [-254..255]
+short B[]={2,-3,4,200,6,7,8,8}; // values range [-254..255]
+short C[]={3,4,5,6,7,8,9,8}; // values range [-254..255]
+short D[]={1,3,2,5,6,7,8,8}; // values range [-2^16+1..2^16]
 
 
 
 int main()
 {
-	int* a = A;
-	int* b = B;
+	short* a = A;
+	short* b = B;
 	sum(a, b); // store result in *(int a)
 
-	int* c = C;
-	int* d = D;
+	short* c = C;
+	short* d = D;
 	sum(c, d); // store result in *(int c)
 	
 	mul(a, c); // store result in *(int a)
@@ -27,6 +28,7 @@ int main()
 	for(int i=0; i < 8; i++)
 		printf(" | %d", a[i]);
 	printf("\n");
+	system("echo 'hello, world'");
 
 	return 1;
 }
